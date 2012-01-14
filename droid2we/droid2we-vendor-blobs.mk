2ea9c5eb4a14fa5065585713353235bb0b7c2d02
emccann <emccann@cs.uml.edu>
2012-01-14 11:39:22 -0500

    adding some missing stuff from droid2we proprietary files

--- droid2we/droid2we-vendor-blobs.mk
+++ droid2we/droid2we-vendor-blobs.mk
@@ -193,8 +193,10 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/droid2we/proprietary/usr/keychars/sholes-keypad.kcm.bin:/system/usr/keychars/sholes-keypad.kcm.bin \
     vendor/motorola/droid2we/proprietary/usr/keychars/sholes-keypad.kcm.bin:/system/usr/keychars/qwerty.kcm.bin \
     vendor/motorola/droid2we/proprietary/usr/keychars/sholes-keypad.kcm.bin:/system/usr/keychars/qwerty2.kcm.bin \
+	vendor/motorola/droid2we/proprietary/usr/keychars/sholes-keypad.kcm.bin:/system/usr/keychars/qtouch-touchscreen.kcm.bin \
     vendor/motorola/droid2we/proprietary/usr/keylayout/sholes-keypad.kl:/system/usr/keylayout/sholes-keypad.kl \
     vendor/motorola/droid2we/proprietary/usr/keylayout/sholes-keypad.kl:/system/usr/keylayout/qwerty.kl \
+	vendor/motorola/droid2we/proprietary/usr/keylayout/sholes-keypad.kl:/system/usr/keylayout/qtouch-touchscreen.kl \
     vendor/motorola/droid2we/proprietary/usr/keylayout/cpcap-key.kl:/system/usr/keylayout/cpcap-key.kl \
     vendor/motorola/droid2we/proprietary/xbin/backup:/system/xbin/backup \
     vendor/motorola/droid2we/proprietary/xbin/drm1_func_test:/system/xbin/drm1_func_test \
